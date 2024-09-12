65a4b130ecc4f604de2c15a5091b9febcb61ae38
Richard Hape <97002851+richape@users.noreply.github.com>
2024-09-12 12:14:03 +1200

    Add files via upload
    initial commit

---
+++ index.py
@@ -0,0 +1,11 @@
+# Integer (whole number)
+age = 39
+
+# Floating-point number (decimal)
+height = 1.81
+
+# String (text)
+name = "Richard"
+
+print(age, height, name) # This prints output: age, height, name
+
