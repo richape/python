c2919c6cbec1ec508954136a6f0ad37de38b3345
Richard <richard.hape@devacademy.co.nz>
2024-09-12 12:35:12 +1200

    basic operations, loops & functions

---
+++ index.py
@@ -0,0 +1,51 @@
+# Integer (whole number)
+age = 39
+
+# Floating-point number (decimal)
+height = 1.81
+
+# String (text)
+name = "Richard"
+
+print(age, height, name) # This prints output: age, height, name
+
+# Basic Operations
+a = 10
+b = 5
+
+# Addition
+print(a + b) # Output: 15
+
+# Subtraction
+print(a - b) # Output: 5
+
+# Multiplication
+print(a * b) # Output: 50
+
+# Division
+print(a / b) # Output: 2
+
+# if statements (Conditionals)
+age = 39
+
+if age >= 18:
+  print("You are an adult")
+else:
+  print("You are still a child")
+
+# Loops
+# Example loop
+for i in range(5):
+  print(i)
+
+# Example while loop
+x = 0
+while x < 5:
+  print(x)
+  x += 1
+
+# Functions
+def greet(name):
+  print("Hello, " + name)
+
+  greet("Richard") # Output: Hello, Richard
